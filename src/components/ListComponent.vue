525db710e0e2dc7191d82730df0bc1c9adfd5400
Tom-Cedric <tom-cedric.gottschlich@gmx.net>
2021-10-25 19:31:20 +0200

    Switch from card to table visualization

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -2,35 +2,35 @@
     <div class="list">
         <h3>Deine Liste:</h3>
         <div class="container">
-            <div v-for="item in dataJson" :key="item.id" class="list-element">
-                <div class="row">
-                    <div class="col"></div>
-                    <div class="col">
-                        <div class="card" style="width: 100%; margin-bottom: 1em;">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ item.name }}</h5>
-                                <p>
-                                    <span>Typ:</span> {{ item.typ }}
-                                </p>
-                                <p>
-                                    <span>Bewertung:</span> {{ item.score }}
-                                </p>
-                                <button v-if="item.done === 1" type="button" class="btn btn-secondary btn-sm">
-                                    <span class="material-icons" style="vertical-align: middle;">
-                                        radio_button_unchecked
-                                    </span>
-                                </button>
-                                <button v-else type="button" class="btn btn-success btn-sm" disabled>
-                                    <span class="material-icons" style="vertical-align: middle;">
-                                        check_circle_outline
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col"></div>
-                </div>
-            </div>  
+            <table class="table">
+                <thead>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Typ</th>
+                    <th>Bewertung</th>
+                    <th>Abgeschlossen?</th>
+                </thead>
+                <tbody>
+                    <tr v-for="item in dataJson" :key="item.id">
+                        <td scope="row">{{ item.id }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.typ }}</td>
+                        <td>{{ item.score }} / 5</td>
+                        <td>
+                            <button v-if="item.done === 1" type="button" class="btn btn-secondary btn-sm">
+                                <span class="material-icons" style="vertical-align: middle;">
+                                    radio_button_unchecked
+                                </span>
+                            </button>
+                            <button v-else type="button" class="btn btn-success btn-sm" disabled>
+                                <span class="material-icons" style="vertical-align: middle;">
+                                    check_circle_outline
+                                </span>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
